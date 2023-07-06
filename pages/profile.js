1f1da42669966ae1c271f326401bb3083439b3a2
MehrabD20 <mehrab20@gmail.com>
2023-07-06 11:43:48 +0330

    prfile page data fetching handled by useSWR hook

--- pages/profile.js
+++ pages/profile.js
@@ -1,34 +1,25 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import useSWR from "swr";
+
+const fetcher = async (api) => {
+   const { data } = await axios.get(api);
+   return data;
+};
 
 const Profile = () => {
-   const [loading, setLoading] = useState(true);
-   const [userData, setUserData] = useState({});
-   const [error, setError] = useState("");
-
-   useEffect(() => {
-      axios
-         .get("http://localhost:4000/profile")
-         .then(({ data }) => {
-            setLoading(false);
-            setUserData(data);
-         })
-         .catch(({ message }) => {
-            setLoading(false);
-            setUserData({});
-            setError(message);
-         });
-   }, []);
-
-   if (loading) return <div>loading...</div>;
+   const { data, error } = useSWR("getUserData", () =>
+      fetcher("http://localhost:4000/profile"),
+   );
 
    if (error) return <div>error: {error}</div>;
+
+   if (!data) return <div>loading...</div>;
 
    return (
       <div>
          <h1>User data:</h1>
          <p>
-            name: {userData.name} email: {userData.email}
+            name: {data.name} email: {data.email}
          </p>
       </div>
    );
